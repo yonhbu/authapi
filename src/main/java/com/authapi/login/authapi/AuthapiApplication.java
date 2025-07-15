79bdf6f16a214e9ddb6584c0a46f3badec93944c
Juan Vargas <vargasjuan91@gmail.com>
2025-07-14 19:11:21 -0500

    Init del Proyecto: Conexion a PostgreSQL

---
+++ src/main/java/com/authapi/login/authapi/AuthapiApplication.java
@@ -0,0 +1,11 @@
+package com.authapi.login.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthapiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthapiApplication.class, args);
+    }
+}
